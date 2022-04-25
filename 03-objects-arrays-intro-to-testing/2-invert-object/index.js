fa3b1437954650ddf5cda92e4e3433b43dea9db3
Alexander Samarkin <alexgui@yandex.ru>
2022-04-25 23:20:30 +0300

    lesson 3

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,12 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (typeof obj === 'undefined') {
+    return void 0;
+  }
 
+  return Object.entries(obj).reduce((result, [key, value]) => {
+    result[String(value)] = key;
+    return result;
+  }, {});
 }
